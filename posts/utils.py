a50eeeda150cd0435d85f0e866df0f045c4a6f4d
ProLiLPivas <ProLiLPivas>
2019-11-27 19:57:40 +0300

    v 0.0.03 - relationship post/tag update

---
+++ posts/utils.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import  View
+
+from .models import *
+
+
+class ReadObjectMixin:
+    model = None
+    template = None
+
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model , slug__iexact=slug)
+        return render(request, self.template, context={ self.model.__name__.lower() : obj })
+
+
--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-from .models import Post, Tag
+from django.shortcuts import get_object_or_404
+from django.views.generic import  View
 
+from .models import Post, Tag
+from .utils import ReadObjectMixin
 
 
 def home_page(request):
     posts = Post.objects.all()
     return render(request, 'posts/first.html', context={'posts': posts})
 
-def read_post(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'posts/read_post.html', context={'post': post })
+
+class ReadPost(ReadObjectMixin, View):
+    model = Post
+    template = 'posts/read_post.html'
 
 
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'posts/tegs_list.html', context={'tags': tags})
 
-def read_teg(request, slug):
-    tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'posts/read_tag.html', context={'tag': tag })
+class ReadTeg(ReadObjectMixin, View):
+   model = Tag
+   template = 'posts/read_tag.html'
+
+
